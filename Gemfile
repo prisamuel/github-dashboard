af4117e87ecaa39c32781eae47ed3957987bc8ea
Priya Samuel <psamuel@thoughtworks.com>
2015-10-29 14:09:18 +0000

    Priya: Initial commit, dashboard with git news and PR

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+gem 'octokit'
+
+gem 'dashing'
+
+gem 'faraday'
---
+++ jobs/github_prs.rb
@@ -0,0 +1,22 @@
+require 'octokit'
+
+SCHEDULER.every '1m', :first_in => 0 do |job|
+  client = Octokit::Client.new(:access_token => "xxx")
+  my_organization = "xxxx"
+  interesting_repos = ["mt*", "fe*", "local-dev-environment", "docker*"]
+  repos = client.organization_repositories(my_organization).map { |repo| repo.name } .select{|j| interesting_repos.find_index(j)}
+
+  open_pull_requests = repos.inject([]) { |pulls, repo|
+    client.pull_requests("#{my_organization}/#{repo}", :state => 'open').each do |pull|
+      pulls.push({
+        title: pull.title,
+        repo: repo,
+        updated_at: pull.updated_at.strftime("%b %-d %Y, %l:%m %p"),
+        creator: "@" + pull.user.login,
+        })
+    end
+    pulls
+  }
+
+  send_event('prs', { header: "Open Pull Requests", pulls: open_pull_requests })
+end
